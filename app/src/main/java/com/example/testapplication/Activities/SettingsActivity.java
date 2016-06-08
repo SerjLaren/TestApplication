ad06f9d8eb7bcfe0072c24b2269dccb814a09e57
SerjLaren <serjlar@mail.ru>
2016-06-08 19:24:42 +0400

    commit_8

--- app/src/main/java/com/example/testapplication/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/testapplication/Activities/SettingsActivity.java
@@ -1,55 +1,74 @@
 package com.example.testapplication.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.android.colorpicker.ColorPickerDialog;
-import com.android.colorpicker.ColorPickerSwatch;
 import com.example.testapplication.R;
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnColor;
     private final static String COLOR_SELECTED = "colorSelected";
     public Intent returnIntent = new Intent();
-    ColorPickerDialog colorPickerDialog;
+    private ColorPickerDialogBuilder colorPickerDialog;
+    private int colorSelected;
+    private String titleColor, titleColorOK, titleColorCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        int[] colors = {getResources().getColor(R.color.white), getResources().getColor(R.color.pink), getResources().getColor(R.color.purple),
-                getResources().getColor(R.color.deep_purple), getResources().getColor(R.color.indigo), getResources().getColor(R.color.blue),
-                getResources().getColor(R.color.light_blue), getResources().getColor(R.color.cyan), getResources().getColor(R.color.teal),
-                getResources().getColor(R.color.green), getResources().getColor(R.color.light_green), getResources().getColor(R.color.lime),
-                getResources().getColor(R.color.yellow), getResources().getColor(R.color.amber), getResources().getColor(R.color.orange),
-                getResources().getColor(R.color.deep_orange)};
-        btnColor = (Button) findViewById(R.id.btnColor); // Белый
+        btnColor = (Button) findViewById(R.id.btnColor);
         btnColor.setOnClickListener(this);
-        colorPickerDialog = new ColorPickerDialog();
-        colorPickerDialog.initialize(
-                R.string.titleColor, colors, 0, 4, 4);
-        colorPickerDialog.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
-            @Override
-            public void onColorSelected(int color) {
-                returnIntent.putExtra(COLOR_SELECTED, color);
-                setResult(RESULT_OK, returnIntent);
-                finish();
-            }
-        });
+        titleColor = getString(R.string.titleColor);
+        titleColorOK = getString(R.string.titleColorOK);
+        titleColorCancel = getString(R.string.titleColorCancel);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnColor:
-                colorPickerDialog.show(getFragmentManager(), "1");
+                buildColorPicker();
                 break;
             default:
                 break;
         }
     }
+
+    private void buildColorPicker() {
+        colorPickerDialog.with(this)
+                .setTitle(titleColor)
+                .initialColor(R.color.white)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(7)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        colorSelected = selectedColor;
+                    }
+                })
+                .setPositiveButton(titleColorOK, new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        returnIntent.putExtra(COLOR_SELECTED, colorSelected);
+                        setResult(RESULT_OK, returnIntent);
+                        finish();
+                    }
+                })
+                .setNegativeButton(titleColorCancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).build().show();
+    }
+
 }
+
